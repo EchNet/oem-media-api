76aaa83a3482450bd55fb3ffd533962ca755dc74
James Echmalian <ech@ech.net>
2021-03-09 14:42:52 -0500

    Model changes: add Website, elaborate ContentSection.

--- content/models.py
+++ content/models.py
@@ -1,48 +1,53 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+NAME_MAX_LENGTH = 24
 
-class ContentConfiguration(models.Model):
+
+class ContentSlot:
   """
-    Key-value storage for content configurations. 
+    A slot is a position in the page at which to insert content.
+    Enumerate the set of slot types supported by the system.  The set is static.
   """
-
-  # In practice, the key is a serialized website-year-make-model tuple.
-  key = models.TextField(
-      blank=False,
-      db_index=True,
-      null=False,
-      unique=True,
-      verbose_name=_("key"),
-  )
-
-  # Unstructured value.
-  config = models.JSONField()
+  NAMES = ("body", "meta", "header", "footer")
+  CHOICES = ((x, x) for x in NAMES)
 
 
 class ContentSection(models.Model):
-  """ A logical section of the output document.  Its contents may vary. """
+  """
+    A logical section of the output document.  A slot is populated with a list of sections.  The contents of
+      each section depend on random selection from among its variants.  """
 
-  # The name of the content section.
+  # Each content section has a name.
   name = models.CharField(
       blank=False,
       db_index=True,
       null=False,
-      max_length=16,
+      max_length=NAME_MAX_LENGTH,
       unique=True,
       verbose_name=_("section id"),
   )
 
-  # The slot, or area of the page where this section should appear.
+  # The slot in which to place this section. (see ContentSlot above)
   slot = models.CharField(
       blank=False,
-      choices=(("meta", "meta"), ("header", "header"), ("body", "body"), ("footer", "footer")),
+      choices=ContentSlot.CHOICES,
       default="body",
       db_index=True,
       null=False,
-      max_length=16,
+      max_length=NAME_MAX_LENGTH,
       verbose_name=_("slot"),
   )
+
+  # The order relative to other sections in the same slot (lower appears higher).
+  order = models.IntegerField(blank=True, default=0, null=False, verbose_name=_("order"))
+
+  # Optional name of a special group of sections to which this section belongs.
+  # Groups may be added/removed from the selected list in bulk.
+  group = models.CharField(blank=True,
+                           null=True,
+                           max_length=NAME_MAX_LENGTH,
+                           verbose_name=_("group"))
 
 
 class ContentVariant(models.Model):
@@ -74,4 +79,48 @@ class ContentVariant(models.Model):
       default=False,
       null=True,
       verbose_name=("deleted"),
+  )
+
+
+class ContentConfiguration(models.Model):
+  """
+    Key-value storage for content configurations. 
+  """
+
+  # In practice, the key is a serialized website-year-make-model tuple.
+  key = models.TextField(
+      blank=False,
+      db_index=True,
+      null=False,
+      unique=True,
+      verbose_name=_("key"),
+  )
+
+  # Structured configuration:
+  # .sections[]
+  #   .sid       # section id
+  #   .vid       # bound variant id
+  config = models.JSONField()
+
+
+class Website(models.Model):
+  """
+    Content goes into websites, after all.
+  """
+
+  # Also serves as a key
+  domain_name = models.CharField(
+      blank=False,
+      db_index=True,
+      null=False,
+      max_length=NAME_MAX_LENGTH,
+      unique=True,
+      verbose_name=_("domain name"),
+  )
+
+  display_name = models.CharField(
+      blank=False,
+      null=False,
+      max_length=NAME_MAX_LENGTH,
+      verbose_name=_("display name"),
   )
